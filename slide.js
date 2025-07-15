7bcb59627dbebd6f9f96beaf12b2a9e837d016a7
Andreas Nordahl <andnorda@gmail.com>
2025-07-15 15:35:37 +0200

    first set of slides

---
+++ slide.js
@@ -0,0 +1,35 @@
+let state = 0;
+
+if (window.location.search.includes("back")) {
+  window.slideChanges?.forEach((change) => {
+    change.forward();
+    state++;
+  });
+}
+
+const forwardKeys = ["PageDown", "ArrowRight", "ArrowDown"];
+const backwardKeys = ["PageUp", "ArrowLeft", "ArrowUp"];
+
+window.addEventListener("keydown", (e) => {
+  const current = Number(location.pathname.match(/(\d+)(\.html)?$/)?.[1] ?? 0);
+  if (forwardKeys.includes(e.key)) {
+    let change = window.slideChanges?.[state];
+    if (change) {
+      change.forward();
+      state++;
+    } else {
+      window.location.href = current + 1;
+    }
+  } else if (backwardKeys.includes(e.key)) {
+    let change = window.slideChanges?.[state - 1];
+    if (change) {
+      change.backward();
+      state--;
+    } else {
+      const url = new URL(window.location);
+      url.pathname = Math.max(current - 1, 0);
+      url.search = "back";
+      window.location.href = url.toString();
+    }
+  }
+});
